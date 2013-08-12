1315fd8374ab72df02d64ee8257ee8378c244010
Matthew Schranz <schranz.m@gmail.com>
2013-08-12 13:44:14 -0400

    flickr

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
  var SERVICES = {},
-     subscribedServices = [],
+     subscribedServices = [
+      "youtube",
+      "soundcloud",
+      "flickr"
+     ],
      MediaSync = {},
      async = require( "async" ),
      request = require( "request" ),
@@ -11,6 +15,7 @@
 
 KEYS.youtube = "key=AI39si5ZfBxRlydtwyL9VaAA9uOAP5J_HBIKyGJ4mZFMT-IalVyAJ5nyi_xadcONfKwcrSXl9DPFWZx_y1K1ccJ1ZbhpZf_quQ";
 KEYS.soundcloud = "client_id=908c9df6f4cd8299277dc162e1bd0806";
+KEYS.flickr = "api_key=b939e5bd8aa696db965888a31b2f1964"
 
 SERVICES.youtube = function( callback ) {
   request({
@@ -18,7 +23,7 @@ SERVICES.youtube = function( callback ) {
     headers: {
       "X-GData-Key": KEYS.youtube
     },
-    uri: "https://gdata.youtube.com/feeds/api/users/Ezimodner/uploads?alt=json"
+    uri: "https://gdata.youtube.com/feeds/api/users/esltv/uploads?alt=json"
   }, function( err, response, body ) {
 
     if ( err ) {
@@ -47,7 +52,36 @@ SERVICES.soundcloud = function( callback ) {
 };
 
 SERVICES.flickr = function( callback ) {
-  callback( null, [] );
+  var jsonBits = "&format=json&nojsoncallback=flickr";
+  request({
+    method: "GET",
+    uri: "https://secure.flickr.com/services/rest/?method=flickr.people.findByUsername&" + KEYS.flickr +
+         "&username=etherworks" + jsonBits
+  }, function( err, response, body ) {
+    if ( err ) {
+      return callback( err );
+    }
+
+    var user_id = JSON.parse( body ).user.nsid;
+
+    request({
+      method: "GET",
+      uri: "https://secure.flickr.com/services/rest/?method=flickr.photos.search&" + KEYS.flickr +
+           "&user_id=" + user_id + jsonBits
+    }, function( err, response, body ) {
+      if ( err ) {
+        return callback( err );
+      }
+
+      var data = JSON.parse( body );
+
+      if ( data.stat === "fail" ) {
+        return callback( "Unknown user" );
+      }
+
+      callback( null, data.photos.photo );
+    });
+  })
 };
 
 // What the fuck.
@@ -60,7 +94,7 @@ MediaSync.get = function( req, res ) {
     SERVICES[ service ]( function( err, results ) {
 
       if ( err ) {
-        return res.json( 500, { status: "failure", reason: "retrieving data for " + service " returned an error" } );
+        return res.json( 500, { status: "failure", reason: "retrieving data for " + service  + " returned an error" } );
       }
 
       results = {
@@ -84,14 +118,17 @@ MediaSync.get = function( req, res ) {
 
 module.exports = function( app, options ) {
 
-  // Setup any API keys
-  for ( var key in options.keys ) {
-    if ( options.keys.hasOwnProperty( key ) ) {
-      if ( key === "rackspace" ) {
-        KEYS[ key ] = options.keys[ key ];
-      }
+  options = options || {};
 
-      subscribedServices.push( key );
+  if ( options.serviceKeys ) {
+    // Rather than use defaults, overwrite
+    subscribedServices = [];
+    // Setup any API keys
+    for ( var key in options.serviceKeys ) {
+      if ( options.serviceKeys.hasOwnProperty( key ) ) {
+        KEYS[ key ] = options.serviceKeys[ key ];
+        subscribedServices.push( key );
+      }
     }
   }
 
